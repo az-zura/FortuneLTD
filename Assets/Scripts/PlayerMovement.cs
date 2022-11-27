7ace40d4f2e00916a145e3d191b145e6e073aecc
Florian Decker <flo122012@gmail.com>
2022-11-27 21:48:53 +0100

    Add character Add animation script for ghosts Create Player Prefab Add Movement Interpolation in Playermovemet

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,17 +7,22 @@ public class PlayerMovement : MonoBehaviour
      public CharacterController controller;
         [SerializeField] private float speed = 4f; 
         [SerializeField] private CapsuleCollider _collider;
+        [SerializeField] private float directionInterpolation;
+
+        private Vector3 direction = new Vector3();
+        
         
         void Update()
         {
             float horizontal = Input.GetAxisRaw("Horizontal");
             float vertical = Input.GetAxisRaw("Vertical");
-       
-            Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+            
+            
+            direction = Vector3.Lerp(direction,new Vector3(horizontal, 0, vertical).normalized,directionInterpolation);
        
             if (direction.magnitude >= 0.1f)
             { 
-                controller.Move(direction * (speed * Time.deltaTime));
+                controller.Move(direction.normalized * (speed * Time.deltaTime));
             }
        
             if (Input.GetKey(KeyCode.Space))
