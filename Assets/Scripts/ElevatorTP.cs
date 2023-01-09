ec528f5de47578c67a123828bca49892561f8ce1
az-zura <anna_lena@naver.com>
2023-01-09 12:45:26 +0100

    added elevator

---
+++ Assets/Scripts/ElevatorTP.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorTP : MonoBehaviour
+{
+    [SerializeField] private bool up;
+    [SerializeField] private GameObject player;
+    private void OnTriggerEnter(Collider other)
+    {
+        CharacterController playerController = player.GetComponent<CharacterController>();
+        Vector3 movement;
+        if (up)
+        {
+            movement = new Vector3(0, 34, 3);
+        }
+        else
+        {
+            movement = new Vector3(0, -34, 5);
+
+        }
+        playerController.Move(movement);
+    }
+}
