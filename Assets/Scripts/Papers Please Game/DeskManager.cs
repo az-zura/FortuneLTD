5aead282560e57e1437935f147e25636798a4400
az-zura <anna_lena@naver.com>
2023-01-13 10:39:33 +0100

    akten

--- Assets/Scripts/Papers Please Game/DeskManager.cs
+++ Assets/Scripts/Papers Please Game/DeskManager.cs
@@ -5,18 +5,35 @@ using UnityEngine;
 
 public class DeskManager : MonoBehaviour
 {
-    [SerializeField] private List<GameObject> folders;
+    [SerializeField] private List<GameObject> foldersGameobjects;
+    private List<Akte> akten;
     [SerializeField] private GameObject _backgroundAkte;
     [SerializeField] private GameObject closedFolder;
 
     [SerializeField] private Camera folderCamera;
     [SerializeField] private Camera deskCamera;
+
+    private int currentFolderToDo = 0;
+    
+    private void Start()
+    {
+        GetAktenFromFolderGameObjects();
+    }
+
+    private void GetAktenFromFolderGameObjects()
+    {
+        foreach (var currentAkte in foldersGameobjects)
+        {
+            Akte tmpakte = new Akte(currentAkte);
+        }
+    }
+    
     public void ObjectHit(string hitObject)
     {
         if (hitObject.Equals("FolderUnopened"))
         {        
             _backgroundAkte.SetActive(true);
-            folders[0].SetActive(true);
+            foldersGameobjects[currentFolderToDo].SetActive(true);
             closedFolder.SetActive(false);
             
             deskCamera.gameObject.SetActive(false);
