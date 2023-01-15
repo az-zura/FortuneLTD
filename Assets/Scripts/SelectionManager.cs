171e58696fdec62c2106e0a8fc5ebab9c26c279e
Lisa Keil <>
2023-01-15 18:46:37 +0100

    glowing object when hovering

---
+++ Assets/Scripts/SelectionManager.cs
@@ -0,0 +1,41 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+
+public class SelectionManager : MonoBehaviour
+{
+    [SerializeField] private string selectableTag = "Selectable";
+    [SerializeField] private Material highlightMaterial;
+    
+    private Material defaultMaterial;
+    private Transform _selection;
+    
+    private void Update()
+    {
+        if (_selection != null)
+        {
+            var selectionRenderer = _selection.GetComponent<Renderer>();
+            //selectionRenderer.material = defaultMaterial;
+            selectionRenderer.materials = new[] {defaultMaterial};
+            _selection = null;
+        }
+        
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            var selection = hit.transform;
+            if (selection.CompareTag(selectableTag))
+            {
+                var selectionRenderer = selection.GetComponent<Renderer>();
+                if (selectionRenderer != null)
+                {
+                    defaultMaterial = selectionRenderer.material;
+                    //selectionRenderer.material = highlightMaterial;
+                    selectionRenderer.materials = new[] {defaultMaterial, highlightMaterial};
+                }
+
+                _selection = selection;
+            }
+        }
+    }
+}
