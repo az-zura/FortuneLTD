b8d01276a6814274413fda831eb226c0a45fb354
Lisa Keil <>
2023-01-31 17:05:50 +0100

    more than one conversation with the npc is possible

--- Assets/Scripts/Conversation.cs
+++ Assets/Scripts/Conversation.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,100 +24,92 @@ public class Conversation : SequentialEvent
                 ids.Add(st.id);
         }
 
-        if (ids.Count <= 0)
-        {
-            return;
-        }
-        
-        // TODO
-        // currentConversation = ids.First();
-        // ids.RemoveAt(0);
-
         // they are talking to each other
-        int n = speakersAndText.Length;
+        //int n = speakersAndText.Length;
         List<SpecificNpcController> controllers = new List<SpecificNpcController>();
         List<GhostAnimation> animations = new List<GhostAnimation>(); 
         List<NPC_Locomotion> locomotions = new List<NPC_Locomotion>();
         List<GameObject> activeSpeakers = new List<GameObject>();
-        
-        for (int i = 0; i < n; i++)
+        List<SpeakerTextManager> nextST = new List<SpeakerTextManager>();
+
+        int n = ids.Count;
+        for (int m = 0; m < n; m++)
         {
-            /*TODO if (speakersAndText[i].id != currentConversation)
+            if (ids.Count <= 0)
             {
-                continue;
-            }*/
+                return;
+            }
+
+            currentConversation = ids.First();
+            ids.RemoveAt(0);
             
-            SpecificNpcController c = speakersAndText[i].speaker.GetComponentInChildren<NpcControllerRandom>();
-            
-            if (!controllers.Contains(c) && c != null)
+            nextST.Clear();
+            nextST.AddRange(Array.FindAll(speakersAndText.ToArray(), s => s.id == currentConversation));
+
+            for (int i = 0; i < nextST.Count; i++)
             {
-                controllers.Add(c);
+                SpecificNpcController c = nextST[i].speaker.GetComponentInChildren<NpcControllerRandom>();
+
+                if (!controllers.Contains(c) && c != null)
+                {
+                    controllers.Add(c);
+                }
+
+                GhostAnimation a = nextST[i].speaker.GetComponentInChildren<GhostAnimation>();
+                if (!animations.Contains(a) && a != null)
+                {
+                    animations.Add(a);
+                }
+
+                NPC_Locomotion l = nextST[i].speaker.GetComponentInChildren<NPC_Locomotion>();
+                if (!locomotions.Contains(l) && l != null)
+                {
+                    locomotions.Add(l);
+                }
+
+                GameObject s = nextST[i].speaker;
+                if (!activeSpeakers.Contains(s) && s != null)
+                {
+                    activeSpeakers.Add(s);
+                }
+            }
+
+            foreach (var s in activeSpeakers)
+            {
+                AddEventItem(new ToggleActiveAction(s, true));
+            }
+
+
+            foreach (var c in controllers)
+            {
+                AddEventItem(new SetNpcControllerPossession(true, c));
+                AddEventItem(new NPCGotoAction(c.GetLocomotion, ConversationManager.instance.player, 0, 2));
+                AddEventItem(new NPCLookAtAction(c.GetAnimation, ConversationManager.instance.player.transform));
+            }
+
+
+            AddEventItem(new EventBasedCondition(clickable.TriggerCondition));
+
+            foreach (var st in nextST)
+            {
+                GhostAnimation ani = st.speaker.GetComponentInChildren<GhostAnimation>();
+                if (ani != null)
+                {
+                    AddEventItem(new EmotionAction(ani, st.emotion));
+                }
+
+                AddEventItem(new SpeakAction(ConversationManager.instance.speechbubble, st.speaker, st.text));
             }
             
-            GhostAnimation a = speakersAndText[i].speaker.GetComponentInChildren<GhostAnimation>();
-            if (!animations.Contains(a) && a != null)
+            
+            foreach (var c in controllers)
             {
-                animations.Add(a);
+                AddEventItem(new NPCLookAtAction(c.GetAnimation));
+                //retrun controll to pams npc controller
+                AddEventItem(new SetNpcControllerPossession(false, c));
             }
             
-            NPC_Locomotion l = speakersAndText[i].speaker.GetComponentInChildren<NPC_Locomotion>();
-            if (!locomotions.Contains(l) && l != null)
-            {
-                locomotions.Add(l);
-            }
-
-            GameObject s = speakersAndText[i].speaker;
-            if (!activeSpeakers.Contains(s) && s != null)
-            {
-                activeSpeakers.Add(s);
-            }
-        }
-
-        foreach (var s in activeSpeakers)
-        {
-            AddEventItem(new ToggleActiveAction(s,true));
-        }
-        
-
-        foreach (var c in controllers)
-        {
-            AddEventItem(new SetNpcControllerPossession(true, c));
-            AddEventItem(new NPCGotoAction(c.GetLocomotion,ConversationManager.instance.player,0,2));
-            AddEventItem(new NPCLookAtAction(c.GetAnimation, ConversationManager.instance.player.transform));
-        }
-        
-        
-        AddEventItem(new EventBasedCondition(clickable.TriggerCondition));
-
-        foreach (var st in speakersAndText)
-        {
-            GhostAnimation ani = st.speaker.GetComponentInChildren<GhostAnimation>();
-            if (ani != null)
-            {
-                AddEventItem(new EmotionAction(ani, st.emotion));
-            }
-
-            AddEventItem(new SpeakAction(ConversationManager.instance.speechbubble,st.speaker,st.text));
-        }
-        
-        //TODO Wait for next click then start next conversation --> id
-        
-        //cleanup
-        /*
-         //TODO maybe to this if conversation ends
-         foreach (var s in activeSpeakers)
-        {
-            AddEventItem(new ToggleActiveAction(s,false));
-        }
-        
-        //disable event parent
-        AddEventItem(new ToggleActiveAction(gameObject,false));*/
-        
-        foreach (var c in controllers)
-        {
-            AddEventItem(new NPCLookAtAction(c.GetAnimation));
-            //retrun controll to pams npc controller
-            AddEventItem(new SetNpcControllerPossession(false, c));
+            AddEventItem(new EventBasedCondition(clickable.TriggerCondition));
         }
 
         StartSequentialEvent();
